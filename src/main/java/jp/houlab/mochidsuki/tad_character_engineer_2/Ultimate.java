bdaa39c0371f1e80e13a37751b7e05d1c4060ab3
soma <mrsumomo7@gmail.com>
2024-06-12 10:38:19 +0900

    from Mochiduki MBAM1 add Ultimate system

---
+++ src/main/java/jp/houlab/mochidsuki/tad_character_engineer_2/Ultimate.java
@@ -0,0 +1,19 @@
+package jp.houlab.mochidsuki.tad_character_engineer_2;
+
+import org.bukkit.entity.Player;
+
+import java.util.HashMap;
+
+public class Ultimate {
+    Player player;
+    public Ultimate(Player player){
+        this.player = player;
+    }
+    public void startUlt(){
+        if(V.cardList.containsKey(player)){
+            
+        }else {
+            return;
+        }
+    }
+}
---
+++ src/main/java/jp/houlab/mochidsuki/tad_character_engineer_2/V.java
@@ -0,0 +1,10 @@
+package jp.houlab.mochidsuki.tad_character_engineer_2;
+
+import org.bukkit.entity.ItemDisplay;
+import org.bukkit.entity.Player;
+
+import java.util.HashMap;
+
+public class V{
+    static public HashMap<Player, ItemDisplay> cardList = new HashMap<>();
+}
